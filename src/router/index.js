ca4b73585c1d3c5704cc3e3d2f86f2f60fe5853c
Celina <celinaclm@gmail.com>
2022-09-30 15:05:34 +0200

    index.js fixed

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import AuthView from '../views/AuthView.vue';
+import Home from '../views/HomeView.vue';
+import Login from '../views/LoginView.vue';
+import Signup from '../views/SignupView.vue';
+import Create from '../views/CreateView.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView,
+    name: 'Home',
+    component: Home,
+    meta: {
+      title: 'Home',
+      auth: false,
+    },
   },
   {
-    path: '/auth',
-    name: 'auth',
-    component: AuthView,
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: {
+      title: 'Login',
+      auth: false,
+    },
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup,
+    meta: {
+      title: 'Signup',
+      auth: false,
+    },
+  },
+  {
+    path: '/create',
+    name: 'Create',
+    component: Create,
+    meta: {
+      title: 'Create',
+      auth: true,
+    },
   },
 ];
 
