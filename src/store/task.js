93860095befacd289a784a8b2b9b4b0262af2934
Celina <celinaclm@gmail.com>
2022-09-30 15:04:17 +0200

    task fixed

--- src/store/task.js
+++ src/store/task.js
@@ -1,17 +1,42 @@
 import { defineStore } from 'pinia';
-import supabase from '../supabase/index';
+import supabase from '../supabase';
 
-export default defineStore('tasks', {
+export const useTaskStore = defineStore('tasks', {
   state: () => ({
     tasks: null,
   }),
   actions: {
     async fetchTasks() {
-      const { data: tasks } = await supabase
+      const { data: tasks, error } = await supabase
         .from('tasks')
         .select('*')
         .order('id', { ascending: false });
       this.tasks = tasks;
+      if (error) throw error;
+    },
+    async insertTodo(title, isComplete, userId) {
+      const { error } = await supabase.from('tasks').insert([
+        {
+          title,
+          isComplete,
+          userId,
+        },
+      ]);
+      if (error) throw error;
+    },
+    async editTodo(title, isComplete, taskId) {
+      await supabase
+        .from('tasks')
+        .update({ title, isComplete })
+        .match({ id: taskId });
+      this.fetchTasks();
+    },
+    async deleteTodo(taskId) {
+      await supabase
+        .from('tasks')
+        .delete()
+        .match({ id: taskId });
+      this.fetchTasks();
     },
   },
 });
