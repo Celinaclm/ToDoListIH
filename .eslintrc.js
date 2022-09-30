11ccdb98945f1b9fe0438d7ceda52458283bf9da
Celina <celinaclm@gmail.com>
2022-09-30 15:02:51 +0200

    more slint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vuejs-accesibility/label-has-for': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    "import/prefer-default-export": "off",
   },
 };
